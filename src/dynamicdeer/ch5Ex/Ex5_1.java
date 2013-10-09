d034e1ae200940b29f43449b40562955ce6f0645
dynamicdeer <dynamicdeer@icloud.com>
2013-10-09 23:53:23 +0900

    자바의 정석 5장 연습문

---
+++ src/dynamicdeer/ch5Ex/Ex5_1.java
@@ -0,0 +1,13 @@
+package dynamicdeer.ch5Ex;
+
+public class Ex5_1 {
+
+	public static void main(String[] args) {
+		int[] arr1[];
+		int[] arr2 = {1,2,3,};   //마지막 쉼표는 있어도 상관 없다함;;
+		int[] arr3 = new int[5];
+		//int[] arr4 = new int[5]{1,2,3,4,5};   //대괄호[]에 숫자 넣으면 안됨. 
+		//int arr5[5];  //배열 선언할 때 크기 지정할 수 없음.
+		int[] arr6[] = new int[3][];
+	}
+}
---
+++ src/dynamicdeer/ch5Ex/Ex5_4.java
@@ -0,0 +1,27 @@
+package dynamicdeer.ch5Ex;
+
+public class Ex5_4 {
+	public static void main(String[] args) {
+	
+		int[][] arr ={
+				{5,5,5,5,5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
+		
+		int total = 0;
+		float average = 0;
+		
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				total+= arr[i][j];
+			}
+		}
+		// int/float으로 형변환 해줘야 소수점이 나온다.
+		average = total / (float)(arr.length*arr[0].length);  
+		
+		System.out.println("total="+total);
+		System.out.println("average="+average);
+	}
+}
